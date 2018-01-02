ed25362878a03712ae6f4ef0ee93c91c92339fae
sns95 <35016632+sns95@users.noreply.github.com>
2018-01-01 21:11:59 -0500

    color changing circles
    repeatedly draws an ellipse at the location of the cursor.

--- sketch.js
+++ sketch.js
@@ -1,11 +1,15 @@
 function setup(){
   createCanvas(windowWidth,windowHeight);
+  background('black');
 }
 
 mouseMoved = function() {
-    fill(mouseX, mouseY, mouseY);
-    ellipse(mouseX, mouseY, 10, 10);
+    //red,green, and blue integer values
+    //background('grey');
+    var g = random(255)
+    var b = random(255);
+
+    noStroke();
+    fill(r,g,b);
+    ellipse(mouseX, mouseY, 40, 40);
 };
-
-
-
